ad662f49c9676f30e3f726491fc3a35e3c7c8eac
Ahmed Mohamed <ahmedmohamedalex93@gmail.com>
2022-04-13 00:45:08 +0200

    updated nest template

--- src/app/template/template.controller.ts
+++ src/app/template/template.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from "@nestjs/common";
-import { ITemplateController } from "./models/interfaces/classes/ITemplateController";
 const tag = "template-be:template:controller";
 @Controller("")
-export class TemplateController implements ITemplateController {
+export class TemplateController {
     constructor () {}
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,11 +1,9 @@
 import { TemplateController } from './template.controller';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
 @Module({
     imports: [
         JwtModule.register({ secret: 'super-secret' }),
-        MongooseModule.forFeature([]),
     ],
     controllers: [TemplateController],
 })
